26ea6988da9ecb87d113e1bab1a899f438de99d3
Mahathi Amencherla <mahathiamencherla15@gmail.com>
2020-06-25 18:16:08 +0530

    Ready for heroku

---
+++ server/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+const port = process.env.PORT || 3000;
+
+const app = express();
+
+const publicPath = path.join(__dirname,'..','public');
+
+app.use(express.static(publicPath));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(publicPath, 'index.html'))
+});
+
+app.listen(port, () => {
+    console.log('server is up on', port)
+})
